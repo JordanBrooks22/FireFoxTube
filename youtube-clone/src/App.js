30b15aa26795c098d8fe5e33f7b1b6c8626c9033
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-26 23:09:11 -0500

    header.js file added search icon and menu icon

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Header from "./Header";
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="app">
       <h1>Hello Clever Programmer, lets build a Youtube Clone</h1>
     
+    <Header />
     {/* header -> <Header /> */}
     {/* header -> <Sidebar /> */}
     {/* header -> <Recomended Videos /> */}
--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import MenuIcon from "@material-ui/icons/Menu";
 
 function Header() {
     return (
         <div
             className="header">
                 <h1> I am a header</h1>
+                <MenuIcon />
 
         </div>
     )
