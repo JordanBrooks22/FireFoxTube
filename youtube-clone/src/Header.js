cb70835bc853795cb0e313c2c261c14a89e9a5b1
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-26 23:31:15 -0500

    imported fb and saved

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React from 'react';
 import MenuIcon from "@material-ui/icons/Menu";
-
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 function Header() {
     return (
         <div
             className="header">
                 <h1> I am a header</h1>
                 <MenuIcon />
+                <img
+                className="header_logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
+                alt=""
+                />
+                <input type="text" />
+                <SearchIcon />
+                <VideoCallIcon />
+                <AppsIcons />
+                <NotificationsIcon />
 
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
---
+++ youtube-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDj74jVEjp0vvWHUmj0jnEvzRbddZrgPQM",
+    authDomain: "master-booster-299317.firebaseapp.com",
+    projectId: "master-booster-299317",
+    storageBucket: "master-booster-299317.appspot.com",
+    messagingSenderId: "929934128982",
+    appId: "1:929934128982:web:f674e29338b74291c3c8f7",
+    measurementId: "G-2GFWX3Y366"
+  };
