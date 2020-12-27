55f60a9946089416a761010097c73a04b0dfc9cb
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-26 21:38:40 -0500

    logo gone added to js file

--- my-app/src/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDj74jVEjp0vvWHUmj0jnEvzRbddZrgPQM",
-    authDomain: "master-booster-299317.firebaseapp.com",
-    projectId: "master-booster-299317",
-    storageBucket: "master-booster-299317.appspot.com",
-    messagingSenderId: "929934128982",
-    appId: "1:929934128982:web:f674e29338b74291c3c8f7",
-    measurementId: "G-2GFWX3Y366"
-  };
