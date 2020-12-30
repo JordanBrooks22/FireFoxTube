dd041e8189f87057e0eff755e76b7cffde909939
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-30 09:39:24 -0500

    spaced out icons in header

--- youtube-clone/src/Header.js
+++ youtube-clone/src/Header.js
@@ -19,16 +19,18 @@ function Header() {
             </div>
 
          <div className="header__input">
-         <input type="text" />
-            <SearchIcon />
+         <input placeholder="Search" type="text"/>
+            <SearchIcon className="header__inputButton"/>
             </div>
 
+            <div className="header__icons">
+            <VideoCallIcon className="header__icon"/>
+            <AppsIcon className="header__icon"/>
+            <NotificationsIcon className="header__icon"/>
+             </div>
 
-            <VideoCallIcon />
-            <AppsIcon />
-            <NotificationsIcon />
 
-
+           
 
        
          
