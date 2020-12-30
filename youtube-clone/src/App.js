f4cf4bec9f0e03bdb68420ed65d1abce7910837b
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-12-30 14:18:16 -0500

    sidebar js added icons

--- youtube-clone/src/App.js
+++ youtube-clone/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
-import './App.css';
+import Sidebar from "./Sidebar";
+import RecommendedVideos from "./RecommendedVideos";
+import "./App.css";
 
 function App() {
   return (
@@ -8,11 +10,14 @@ function App() {
     <div className="app">
       <h1>Hello Clever Programmer, lets build a Youtube Clone</h1>
     
-    <Header />
-    {/* header -> <Header /> */}
-    {/* header -> <Sidebar /> */}
-    {/* header -> <Recomended Videos /> */}
-    
+    <Header  />
+   <div className="app__page">
+
+   <Sidebar  />
+   <RecommendedVideos  />
+
+
+   </div>
     
     
     </div>
---
+++ youtube-clone/src/RecommendedVideos.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function RecommendedVideos() {
+    retun (
+        <div>
+            <h1>Videos here</h1>
+
+        </div>
+    )
+}
